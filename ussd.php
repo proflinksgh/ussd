c2bb0c41925742b21e36112bf8b319ff1f95caf6
ProfLinks <48153273+proflinksgh@users.noreply.github.com>
2021-04-20 17:10:40 +0000

    Update ussd.php

--- ussd.php
+++ ussd.php
@@ -1,11 +1,18 @@
 <?php 
 
-  //$text = $_POST['text']; 
-  $text = "";
+  $sessionId = $_POST['sessionId'];
+  $serviceCode = $_POST['serviceCode'];  
+  $phoneNumber = $_POST['phoneNumber'];
+  $text = $_POST['text']; 
 
 
   if($text == ""){
-  	$response = 'CON Welcome to Accra Institute of Technology <br> Select option:<br><br>1. Apply Admission <br>2. View courses <br> 3. Pay fees';
+  	$response = 'Welcome to Accra Institute of Technology \n\n';
+  	$response .= 'Select option:\n';
+  	$response .= '1. Apply Admission\n';
+  	$response .= '2. View courses\n';
+  	$response .= '3. Pay fees\n';
+
   }
 
   echo $response;
