70fb18ac84d7dfb496d97185e8f0b0b32cce4c17
ProfLinks <48153273+proflinksgh@users.noreply.github.com>
2021-04-20 18:22:14 +0000

    Update ussd.php

--- ussd.php
+++ ussd.php
@@ -10,7 +10,7 @@
 //          $phoneNumber = $req->get('phoneNumber');
 //          $text = $req->get('text');
          
-      if($text == ""){
+          if($text == ""){
       	$response = "CON Welcome to Barry Financial Services\n\n";
       	$response .= "Please select option:\n";
       	$response .= "1. Open account\n";
@@ -21,13 +21,17 @@
       
         else if($text == "1"){
        // $_SESSION['open'] = "yes";
+        
       	$response = "CON Open an account\n\n";
       	$response .= "Please select account:\n";
       	$response .= "1. Savings account\n";
       	$response .= "2. Current account\n";
       	$response .= "3. Fixed deposit\n";
+      	$response .= "0. Back\n";
+      }  else if($text == "1*0"){
+       // $_SESSION['open'] = "yes";
+      	$response = "CON Enter full name:t\n\n";
+      	$response .= "0. Back\n";
       }
-    
-      echo $response;
 
 ?>
