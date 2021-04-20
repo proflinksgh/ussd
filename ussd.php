267a1e42ca7197550a8a2b81f48690af62cfa2c9
ProfLinks <48153273+proflinksgh@users.noreply.github.com>
2021-04-20 16:46:44 +0000

    Update ussd.php

--- ussd.php
+++ ussd.php
@@ -5,7 +5,7 @@
 
 
   if($text == ""){
-  	$response = 'Welcome to Accra Institute of Technology <br> Select option:<br><br>1. Apply Admission <br>2. View courses <br> 3. Pay fees';
+  	$response = 'CON Welcome to Accra Institute of Technology <br> Select option:<br><br>1. Apply Admission <br>2. View courses <br> 3. Pay fees';
   }
 
   echo $response;
