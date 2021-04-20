68c383c4c4b54650d16756ae9a60352cb9ef30c8
ProfLinks <48153273+proflinksgh@users.noreply.github.com>
2021-04-20 18:10:54 +0000

    Update ussd.php

--- ussd.php
+++ ussd.php
@@ -5,17 +5,29 @@
   $phoneNumber = $_POST['phoneNumber'];
   $text = $_POST['text']; 
 
-
-  if($text == ""){
-  	$response = "CON Welcome to Barry Financial Services\n\n";
-  	$response .= "Please select option:\n";
-  	$response .= "1. Our services\n";
-  	$response .= "2. Make transaction\n";
-  	$response .= "3. Make enquiry\n";
-    $response .= "4. View branches\n";
-
-  }
-
-  echo $response;
+//          $sessionId = $req->get('sessionId');
+//          $serviceCode = $req->get('serviceCode');
+//          $phoneNumber = $req->get('phoneNumber');
+//          $text = $req->get('text');
+         
+      if($text == ""){
+      	$response = "CON Welcome to Barry Financial Services\n\n";
+      	$response .= "Please select option:\n";
+      	$response .= "1. Open account\n";
+      	$response .= "2. Make deposit\n";
+      	$response .= "3. Make withdrawal\n";
+        $response .= "4. View branches\n";
+      }
+      
+        else if($text == "1"){
+       // $_SESSION['open'] = "yes";
+      	$response = "CON Open an account\n\n";
+      	$response .= "Please select account:\n";
+      	$response .= "1. Savings account\n";
+      	$response .= "2. Current account\n";
+      	$response .= "3. Fixed deposit\n";
+      }
+    
+      echo $response;
 
 ?>
