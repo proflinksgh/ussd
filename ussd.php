23ed9e1d89bf3a13317d0adceb81157de9ceb93f
ProfLinks <48153273+proflinksgh@users.noreply.github.com>
2021-04-21 18:43:17 +0000

    Update ussd.php

--- ussd.php
+++ ussd.php
@@ -35,8 +35,13 @@ $level = count($ussd_string_exploded);
 if($level == 1 && $ussd_string_exploded[0] == ""){
    // display_menu();
  echo "CON Level is: ".$level."String is: ".$ussd_string_exploded[0];
-}else if($level == 1 && $ussd_string_exploded[0] == "1"){
-  display_register_info();
+}
+
+// else if($level == 1 && $ussd_string_exploded[0] == "1"){
+//   display_register_info();
+// }
+else{
+  echo "CON Level is: ".$level."String is: ".$ussd_string_exploded[0];
 }
 
 
