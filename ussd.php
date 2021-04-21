e6cb801ba49ec737571813f0f67ad54659627bb8
ProfLinks <48153273+proflinksgh@users.noreply.github.com>
2021-04-21 18:30:28 +0000

    Update ussd.php

--- ussd.php
+++ ussd.php
@@ -33,8 +33,9 @@ $level = count($ussd_string_exploded);
 
 
 if($level == 1 && $ussd_string_exploded[0] == ""){
-    
     display_menu();
+}else if($level == 1 && $ussd_string_exploded[0] == "1"){
+  display_register_info();
 }
 
 
