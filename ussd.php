095f10cb3c3c84a548cd4550ea4793f590c4f46e
ProfLinks <48153273+proflinksgh@users.noreply.github.com>
2021-04-22 04:16:02 +0000

    Update ussd.php

--- ussd.php
+++ ussd.php
@@ -26,19 +26,19 @@ if($level == 1 && $ussd_string == ""){
 }else if($level == 1 && $ussd_string == "1"){
   display_register_info();
 }
-// else if ($level == 2 && $strl > 10)
-// {
-//    //Store name contact in db
-//     $explode_input = explode (",",$ussd_string);
-//     $name = $explode_input[0];
-//     $contact = $explode_input[1];
+else if ($level == 2 && $strl > 10)
+{
+   //Store name contact in db
+    $explode_input = explode (",",$ussd_string);
+    $name = $explode_input[0];
+    $contact = $explode_input[1];
 
-//     $text = "Your name is: ".$name."\n Your contact is: ".$contact." \n\n 1. Confirm \n 2. Quit";
-//     ussd_proceed($text);
+    $text = "Your name is: ".$name."\n Your contact is: ".$contact." \n\n 1. Confirm \n 2. Quit";
+    ussd_proceed($text);
   
-// }else if($level == 2 && $strl <= 10){
-//     display_register_info();
-// }
+}else if($level == 2 && $strl <= 10){
+    display_register_info();
+}
 
 
 
