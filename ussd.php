bf69ae3deeae0d24165474cce1fba6696a084010
ProfLinks <48153273+proflinksgh@users.noreply.github.com>
2021-04-22 04:43:23 +0000

    Update ussd.php

--- ussd.php
+++ ussd.php
@@ -28,18 +28,16 @@ if($level == 1 && $ussd_string == ""){
   display_register_info();
 }else if ($level == 2 && $strl > 14)
 {
-    $explode_input = explode (",",$ussd_string);
+    $explode_input = explode (",",$ussd_string_exploded );
     $name = $explode_input[0];
     $contact = $explode_input[1];
 
     $text = "Your name is: ".$name."\n Your contact is: ".$contact." \n\n 1. Confirm \n 2. Quit";
     ussd_proceed($text);
   
+}else if($level == 2 && $strl <= 10){
+    display_register_info();
 }
-
-// else if($level == 2 && $strl <= 10){
-//     display_register_info();
-// }
 
 
 
